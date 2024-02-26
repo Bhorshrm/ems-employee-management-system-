6e0cf90e92be4805eb4e9bbafe7a229377029454
bhor123 <bhorshrm@gmail.com>
2024-02-26 21:18:12 +0530

    added constructor for dep injection, is this correct now

--- src/main/java/com/example/ems/controller/Controller.java
+++ src/main/java/com/example/ems/controller/Controller.java
@@ -1,18 +1,27 @@
 package com.example.ems.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.ems.DTO.EmployeeDTO;
 import com.example.ems.service.EmployeeServiceImpl;
-public class Controller{
-    EmployeeServiceImpl es;
-@GetMapping("/employee/{id}")
-public ResponseEntity<EmployeeDTO> getEmployeeById(@PathVariable String id){
-    //retrieve DTO from service
-    //then send it to client in responseEntity
-    EmployeeDTO edto=es.getEmployeeById(id);
-    return ResponseEntity.ok(edto);
-}
+
+public class Controller {
+
+    private EmployeeServiceImpl es;
+
+    @Autowired
+    public Controller(EmployeeServiceImpl es) {
+        this.es = es;
+    }
+
+    @GetMapping("/employee/{id}")
+    public ResponseEntity<EmployeeDTO> getEmployeeById(@PathVariable String id) {
+        // retrieve DTO from service
+        // then send it to client in responseEntity
+        EmployeeDTO edto = es.getEmployeeById(id);
+        return ResponseEntity.ok(edto);
+    }
 }
